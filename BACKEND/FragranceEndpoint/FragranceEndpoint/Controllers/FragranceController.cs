d130130f7ae4b560e66b1e57424e663f3a68937c
zadoriaron <789zadori.aron@gmail.com>
2025-11-26 22:39:20 +0100

    abc

---
+++ BACKEND/FragranceEndpoint/FragranceEndpoint/Controllers/FragranceController.cs
@@ -0,0 +1,46 @@
+﻿using Fragrance_Data.Dto;
+using Fragrance_Data.Models;
+using Fragrance_Data;
+using Microsoft.AspNetCore.Mvc;
+namespace DevOpsProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FragranceController : ControllerBase
+    {
+        FragranceRepository repo;
+
+        public FragranceController(FragranceRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpPost("AddFragrance")]
+        public ActionResult<FragranceModel> AddFragrance([FromBody] FragranceCreateDto dto)
+        {
+            FragranceModel model = new FragranceModel()
+            {
+                Name = dto.Name,
+                Brand = dto.Brand,
+                Price = dto.Price,
+                PictureUrl = dto.PictureUrl
+            };
+
+            repo.Create(model);
+
+            return Ok(model); // visszaküldjük a létrehozott objektumot
+        }
+
+        [HttpGet("GetFragrances")]
+        public IEnumerable<FragranceModel> GetFragrances()
+        {
+            return repo.GetAll().ToList();
+        }
+
+        [HttpDelete]
+        public void DeleteFragrance([FromBody] string id)
+        {
+            repo.Delete(id);
+        }
+    }
+}
